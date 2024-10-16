9b31311096293420f99ad25ffd54cbd5157ff292
Nastik982 <anastasiia.shevchenko1@nure.ua>
2024-10-16 12:02:56 +0300

    ///

--- New_Lab1/__init__.py
+++ New_Lab1/__init__.py
@@ -1,5 +1,15 @@
 import requests
-
+import json
 wrl_string = "https://bank.gov.ua/NBU_Exchange/exchange_site?json&start=20241007&end=20241011&valcode=usd"
 
-requests.get(wrl_string)
+my_response = requests.get(wrl_string)
+
+print(my_response)
+print(my_response.content)
+
+response_json = json.loads(my_response.content)
+
+print(response_json)
+
+for item in response_json:
+    print(item['rate'])
